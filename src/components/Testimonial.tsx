194ca5e1bd622454a717a71c85749d3e24a26fd3
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-08-07 22:47:38 +0200

    Testimonials hover effects added

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -9,7 +9,9 @@ interface Props {
 
 const Testimonial: React.FC<Props> = ({ testimonial }: Props) => {
     return (
-        <div id='Testimonial' className='w-96 -lg:w-2/3 bg-white shadow-xl after:w-96 after:opacity-0 after:shadow-2xl hover:after:opacity-1 flex flex-col items-center p-4'>
+        <div
+            id='Testimonial'
+            className='w-96 -lg:w-2/3 h-[26rem] bg-white shadow-xl flex flex-col items-center p-4 hover:transform hover:scale-105 transition-all duration-300 ease-in-out'>
             <Img src={testimonial.pfpPath} alt='client-profile-pic' className='aspect-square rounded-full w-1/3' />
             <h2 className='font-bold text-xl tracking-wide py-2'>{testimonial.author}</h2>
             <Rating className='py-2' rating={testimonial.rating} />
