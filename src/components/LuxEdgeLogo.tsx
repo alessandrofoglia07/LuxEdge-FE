32588954bee3608958b1c85439ae11413da01e60
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-08-23 14:09:47 +0200

    Added more props to LuxEdgeLogo component

--- src/components/LuxEdgeLogo.tsx
+++ src/components/LuxEdgeLogo.tsx
@@ -3,12 +3,31 @@ import Highlight from './Highlight';
 
 interface Props {
     className?: string;
+    id?: string;
+    style?: React.CSSProperties;
+    lighter?: boolean;
+    monochrome?: boolean;
+    monochromeRev?: boolean;
 }
 
-const LuxEdge: React.FC<Props> = ({ className = '' }: Props) => (
-    <span className={`tracking-tighter font-extrabold ${className}`}>
-        Lux<Highlight effect>Edge</Highlight>
-    </span>
-);
+const LuxEdge: React.FC<Props> = ({ className = '', id, style, lighter = false, monochrome = false, monochromeRev = false }: Props) =>
+    monochrome ? (
+        <span id={id} style={style} className={`tracking-tighter font-extrabold text-slate-100 ${className}`}>
+            Lux
+            <span className='text-gray-900'>Edge</span>
+        </span>
+    ) : monochromeRev ? (
+        <span id={id} style={style} className={`tracking-tighter font-extrabold text-gray-900 ${className}`}>
+            Lux
+            <span className='text-slate-100'>Edge</span>
+        </span>
+    ) : (
+        <span id={id} style={style} className={`tracking-tighter font-extrabold ${className}`}>
+            Lux
+            <Highlight effect={true} lighter={lighter}>
+                Edge
+            </Highlight>
+        </span>
+    );
 
 export default LuxEdge;
