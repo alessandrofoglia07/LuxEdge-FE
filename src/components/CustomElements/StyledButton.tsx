27c72756c429d0d3c0e2560383d7bb402190eae0
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-08-27 20:56:37 +0200

    Fixed StyledButton and StyledInput components

--- src/components/CustomElements/StyledButton.tsx
+++ src/components/CustomElements/StyledButton.tsx
@@ -1,36 +1,33 @@
 import React, { DetailedHTMLProps } from 'react';
-import { useNavigate } from 'react-router-dom';
 
-interface Props extends DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-    /** If set, `onClick` property will not work anymore. */
-    href?: string;
-    classNameOverride?: boolean;
-}
+type ButtonProps = DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
+type AnchorProps = DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>;
+
+type Props = (ButtonProps | AnchorProps) & {
+    as?: 'button' | 'a';
+    secondary?: boolean;
+};
 
 const Button: React.FC<Props> = (props: Props) => {
-    const navigate = useNavigate();
+    const Component = props.as || 'button';
 
     const newProps = { ...props };
 
-    if (newProps.href) {
-        newProps.onClick = () => navigate(newProps.href!);
-        delete newProps.href;
-    }
-
+    delete newProps.as;
+    delete newProps.secondary;
     delete newProps.className;
-    delete newProps.children;
-    delete newProps.classNameOverride;
 
     return (
-        <button
+        <Component
             className={
-                props.classNameOverride
-                    ? props.className
-                    : `font-bold text-lg px-4 py-2 select-none rounded-xl text-white bg-primary-base hover:bg-primarytext-primary-hover mx-4 ${props.className}`
+                props.secondary
+                    ? `font-bold text-lg lg:mx-4 sm:mx-2 flex items-center gap-1 select-none ${props.className}`
+                    : `font-bold text-lg px-4 py-2 select-none rounded-xl text-white bg-primary-base hover:bg-primary-hover mx-4 ${props.className}`
             }
-            {...newProps}>
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            {...(newProps as any)}>
             {props.children}
-        </button>
+        </Component>
     );
 };
 
