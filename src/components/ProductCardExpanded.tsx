e6b14b2867891daabe0f288d21a6c5b520aa9a22
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-10-19 16:50:22 +0200

    Bug fixed in ProductCardExpanded component

--- src/components/ProductCardExpanded.tsx
+++ src/components/ProductCardExpanded.tsx
@@ -24,12 +24,6 @@ const ProductCard: React.FC<Props> = (_: Props) => {
         setProduct(_.product);
     }, [_]);
 
-    const Container = ({ children }: { children: React.ReactNode }) => (
-        <div id='ProductCard' className='md:w-96 flex flex-col items-center md:min-w-[24rem] md:h-fit w-min bg-white rounded-lg p-4'>
-            {children}
-        </div>
-    );
-
     const handleFavorite = async () => {
         // if product is in favs
         if (_.isFavorite) {
@@ -46,14 +40,20 @@ const ProductCard: React.FC<Props> = (_: Props) => {
     };
 
     return (
-        <Container>
+        <div id='ProductCard' className='md:w-96 flex flex-col items-center md:min-w-[24rem] md:h-fit w-min bg-white rounded-lg p-4'>
             <div id='top' className='md:w-full md:h-1/2 w-full h-64 group'>
                 <a draggable={false} className='w-full h-full md:pt-4' href={`/products/${toPlural(product.category)}/${product.name}`}>
                     <Img className='w-full h-full rounded-md border-slate-200 drop-shadow-xl border-2' src={toUrl(product.imagePath)} alt={product.name} />
                 </a>
                 <div className='absolute flex group-hover:opacity-100 transition-opacity duration-300 md:opacity-0 p-2 flex-col -translate-y-full text-end items-end rounded-b-lg'>
-                    <button id='favs' className={`w-10 aspect-square grid place-items-center bg-black bg-opacity-60 transition-all duration-200 rounded-lg `} onClick={handleFavorite}>
-                        <HeartIcon className={`w-6 h-6 ${_.isFavorite ? 'text-red-500 hover:text-red-600' : 'text-slate-200 hover:text-slate-50'}`} />
+                    <button
+                        id='favs'
+                        className={`w-10 aspect-square grid place-items-center bg-black bg-opacity-60 transition-all duration-200 rounded-lg ${
+                            _.isFavorite ? 'text-red-500 hover:text-red-600' : 'text-slate-200 hover:text-slate-50'
+                        }`}
+                        onClick={handleFavorite}
+                    >
+                        <HeartIcon className='w-6 h-6' />
                     </button>
                 </div>
             </div>
@@ -67,7 +67,7 @@ const ProductCard: React.FC<Props> = (_: Props) => {
                 </a>
                 <h6 className='font-semibold tracking-wide'>{toPrice(product.price)}</h6>
             </div>
-        </Container>
+        </div>
     );
 };
 
