34638c5cd6c9583a30117187a74a5a7795d1f058
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-11-10 22:28:51 +0100

    Add cancel payment functionality to CheckoutCancelPage component

--- src/pages/checkout/CheckoutCancelPage.tsx
+++ src/pages/checkout/CheckoutCancelPage.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Navigate, useSearchParams } from 'react-router-dom';
+import authAxios from '@/api/authAxios';
 
 const CheckoutCancelPage: React.FC = () => {
-    return (
-        <div id='CheckoutCancelPage'>
-            <h1>CheckoutCancelPage</h1>
-        </div>
-    );
+    const [searchParams] = useSearchParams();
+
+    const calcelPayment = async () => {
+        const session_id = searchParams.get('session_id');
+        if (!session_id) return;
+        try {
+            await authAxios.delete(`/payment/cancel?session_id=${session_id}`);
+        } catch (err: unknown) {
+            console.error(err);
+        }
+    };
+
+    useEffect(() => {
+        calcelPayment();
+    }, [searchParams]);
+
+    return <Navigate to='/cart' />;
 };
 
 export default CheckoutCancelPage;
